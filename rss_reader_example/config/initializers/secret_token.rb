c1eb575dbd5ecbfc862799e3ee4043757fa8bef3
Joe Letizia <joe.letizia@gmail.com>
2013-03-30 16:13:10 -0400

    added scaffold for entire project

---
+++ rss_reader_example/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RssReaderExample::Application.config.secret_token = '051ab0373a498e8501b5247f1e95de8e9c31b868f804d909cf2f58a4ea83fc5aa7e3fe5048bcce9ae26d418a5bed74f0bd166ea89d80ef53f50380f2b2944f28'
