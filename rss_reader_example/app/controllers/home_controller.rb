09ace237217623d74a688714660f241bdccf053b
Joe Letizia <joe.letizia@gmail.com>
2013-03-31 13:11:34 -0400

    properly retrieving and sending preview

--- rss_reader_example/app/controllers/home_controller.rb
+++ rss_reader_example/app/controllers/home_controller.rb
@@ -9,11 +9,13 @@ class HomeController < ApplicationController
 
   def posts
     feed = Feedzirra::Feed.fetch_and_parse(params[:url])
-    feed.entries.first(1).each do |entry|
-      @post = OpenStruct.new
-      @post.title = entry.title
-      @post.author = entry.author
-      @post.preview = entry.content
+    @posts = []
+    feed.entries.first(10).each do |entry|
+      post = OpenStruct.new
+      post.title = entry.title
+      post.author = entry.author
+      post.preview = entry.content.gsub(%r{</?[^>]+?>}, '').split[0..15].join(" ") + "..."
+      @posts << post
     end
     render 'posts.js.erb'
   end
