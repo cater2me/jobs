6f46e2fe819a413c9552580ae38eea526b628e41
Joe Letizia <joe.letizia@gmail.com>
2013-04-02 18:02:49 -0400

    added test for a bad URL

--- rss_reader_example/spec/requests/posts_spec.rb
+++ rss_reader_example/spec/requests/posts_spec.rb
@@ -2,11 +2,18 @@ require 'spec_helper'
 
 describe "Posts" do
   describe "Home" do
-    it "should return " do
+    it "should return articles for a valid url" do
       visit '/'
       fill_in "url", :with => "http://feeds.feedburner.com/PaulDixExplainsNothing"
       click_button "get_posts"
       page.has_content? "Articles"
+    end
+
+    it "should return an error message for an invalid URL" do
+      visit '/'
+      fill_in "url", :with => "http://feeds.feedburner123.com/PaulDixExplainsNothing"
+      click_button "get_posts"
+      page.has_content? "The URL provided is not a valid RSS/Atom feed."
     end
   end
 end
