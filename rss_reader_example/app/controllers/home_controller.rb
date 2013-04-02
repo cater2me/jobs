37a6d4c0330dd1413266c3e8866b08a427267878
Joe Letizia <joe.letizia@gmail.com>
2013-04-02 15:32:19 -0400

    added ability to hover over links and see preivew, and implemented the modal window to display the article.

--- rss_reader_example/app/controllers/home_controller.rb
+++ rss_reader_example/app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 require 'feedzirra'
 require 'open-uri'
 require 'ostruct'
+require 'base64'
 
 class HomeController < ApplicationController
   def index
@@ -14,9 +15,27 @@ class HomeController < ApplicationController
       post = OpenStruct.new
       post.title = entry.title
       post.author = entry.author
+      post.feed_url = feed.feed_url
+      post.url = entry.url
       post.preview = entry.content.gsub(%r{</?[^>]+?>}, '').split[0..15].join(" ") + "..."
+      post.content = entry.content
       @posts << post
     end
     render 'posts.js.erb'
+  end
+
+  def post
+    feed_url = Base64.decode64(params[:feed_url])
+    post_title = Base64.decode64(params[:post_title])
+
+    logger.debug "feed_url: #{feed_url}"
+    logger.debug "post_title: #{post_title}"
+
+    feed = Feedzirra::Feed.fetch_and_parse(feed_url)
+
+    posts = feed.entries.first(10).select {|entry| entry.title == post_title}
+    @post = posts.first
+    logger.debug "fetched: #{@post.title}"
+    render 'post.js.erb'
   end
 end
--- rss_reader_example/config/routes.rb
+++ rss_reader_example/config/routes.rb
@@ -5,6 +5,7 @@ RssReaderExample::Application.routes.draw do
   root :to => "home#index"
 
   match '/posts' => 'home#posts', :as => :posts
+  match '/posts/:feed_url/:post_title' => 'home#post', :as => :post
   devise_for :users
   resources :users
 
