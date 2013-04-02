7af100a8a036638a4991033e65ccc5dbabe4123d
Joe Letizia <joe.letizia@gmail.com>
2013-04-02 17:53:25 -0400

    added some validation on teh URL and changed post objects to be hashes instead of structs.

--- rss_reader_example/app/controllers/home_controller.rb
+++ rss_reader_example/app/controllers/home_controller.rb
@@ -4,21 +4,20 @@ require 'ostruct'
 require 'base64'
 
 class HomeController < ApplicationController
-  def index
-    @users = User.all
-  end
 
   def posts
     feed = Feedzirra::Feed.fetch_and_parse(params[:url])
+    
+    if feed.try(:entries).nil?
+      render 'invalid_url.js.erb' 
+      return
+    end
+    
     @posts = []
     feed.entries.first(10).each do |entry|
-      post = OpenStruct.new
-      post.title = entry.title
-      post.author = entry.author
-      post.feed_url = feed.feed_url
-      post.url = entry.url
-      post.preview = entry.content.gsub(%r{</?[^>]+?>}, '').split[0..15].join(" ") + "..."
-      post.content = entry.content
+      post = {:title => entry.title, :author => entry.author, :feed_url => feed.feed_url, :url => entry.url,
+        :preview => entry.content.gsub(%r{</?[^>]+?>}, '').split[0..15].join(" ") + "..."}
+
       @posts << post
     end
     render 'posts.js.erb'
@@ -28,14 +27,12 @@ class HomeController < ApplicationController
     feed_url = Base64.decode64(params[:feed_url])
     post_title = Base64.decode64(params[:post_title])
 
-    logger.debug "feed_url: #{feed_url}"
-    logger.debug "post_title: #{post_title}"
-
     feed = Feedzirra::Feed.fetch_and_parse(feed_url)
 
     posts = feed.entries.first(10).select {|entry| entry.title == post_title}
     @post = posts.first
-    logger.debug "fetched: #{@post.title}"
+
     render 'post.js.erb'
   end
+
 end
