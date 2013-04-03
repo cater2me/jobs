9c7e14d0dae997b73ae384099830205c70045350
Joe Letizia <joe.letizia@gmail.com>
2013-04-03 18:38:08 -0400

    forgot a quote

--- rss_reader_example/app/models/feed.rb
+++ rss_reader_example/app/models/feed.rb
@@ -15,7 +15,7 @@ class Feed < ActiveRecord::Base
     feed.entries.first(10).each do |entry|
       # strip tags from preview
       summary = entry.summary || "No summary was provided by the feed."
-      post = Post.new(:preview => summary.gsub( %r{</?[^>]+?>}, '' ), :author => entry.author || "...someone, :content => entry.content, :title => entry.title, :url => entry.url)
+      post = Post.new(:preview => summary.gsub( %r{</?[^>]+?>}, '' ), :author => entry.author || "...someone", :content => entry.content, :title => entry.title, :url => entry.url)
       post.feed = self
       post.save if recently_fetched.select {|rec| rec.url == post.url}.count == 0
     end
