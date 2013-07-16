5f21fe36639637d61e5f3a65919a2058b62d6034
Kashyap Bhatt <kashyap.bhatt15@gmail.com>
2013-07-16 02:41:06 -0400

    complete refactor example

--- refactor_example/template_spec.rb
+++ refactor_example/template_spec.rb
@@ -6,5 +6,25 @@ describe Template do
   it "should substitute %CODE% and %ALTCODE% in the template" do
     template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
   end
-  
+
+  it "should substitute %CODE% but not %ALTCODe% in the template" do
+    template('Code is %CODE%; alt code is %ALTCODe%', '5678901234').should == 'Code is 5678901234; alt code is %ALTCODe%'
+  end
+
+  it "should substitute %CODE% and %ALTCODE% in the template twice" do
+    template('Code is %CODE%; alt code is %ALTCODE%, Again Code is %CODE%; alt code is %ALTCODE%',
+     '5678901234').should == 'Code is 5678901234; alt code is 56789-012, Again Code is 5678901234; alt code is 56789-012'
+  end
+
+	it "should substitute %CODE% and %ALTCODE% in the template even if number is <4" do
+    template('Code is %CODE%; alt code is %ALTCODE%', '124').should == 'Code is 124; alt code is 124-'
+  end
+
+  it "should substitute %CODE% and %ALTCODE% in the template even if number is >4 but <7" do
+    template('Code is %CODE%; alt code is %ALTCODE%', '1234509').should == 'Code is 1234509; alt code is 12345-09'
+  end
+
+  it "should substitute %CODE% only in the template" do
+    template('Code is %CODE%', '12341234').should == 'Code is 12341234'
+  end 
 end
