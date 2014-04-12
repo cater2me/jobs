c3ed0583a0ebdfded99f72364f92407d7723cb55
David Goodman <goodman.david.h@gmail.com>
2014-04-12 15:37:11 -0700

    adds regex to change first code

--- refactor_example/template.rb
+++ refactor_example/template.rb
@@ -1,17 +1,9 @@
-module Template
-  def template(source_template, req_id)
-    template = String.new(source_template)
+#module Template
+  def change(source_template, req_id)
+    #template = String.new(source_template) ##No need because source_tempalte is being passed as a string
 
     # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+    source_template.gsub!(/%CODE%/,req_id)
 
     # Substitute for %ALTCODE%
     template_split_begin = template.index("%ALTCODE%")
@@ -23,4 +15,8 @@ module Template
     altcode = code[0..4] + "-" + code[5..7]
     template_part_one + altcode + template_part_two
   end
-end
+#end
+
+
+
+ p change('Code is %CODE%; alt code is %ALTCODE%', '5678901234') == 'Code is 5678901234; alt code is 56789-012'
