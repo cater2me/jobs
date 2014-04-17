8f2ec960f6ff835ed0269df8c06e8cdeda2e46ec
Alex Reichert <alexreichert621@gmail.com>
2014-04-16 23:06:22 -0700

    add options to open in new window or overlay

--- rss_reader_example/simple_rss/config/application.rb
+++ rss_reader_example/simple_rss/config/application.rb
@@ -19,6 +19,8 @@ module SimpleRss
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    # config.action_dispatch.default_headers.merge!({'X-Frame-Options' => 'ALLOWALL'})
+    
     config.generators do |g|
       g.test_framework :rspec, 
         :fixtures => true, 
@@ -28,5 +30,6 @@ module SimpleRss
         :controller_specs => true, 
         :request_specs => true
     end
+  
   end
 end
