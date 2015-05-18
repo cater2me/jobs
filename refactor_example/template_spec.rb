597fae700755d27692eb3162fa52bf0a07d9f2ec
Loren Hale <loren.hale@gmail.com>
2015-05-18 16:45:08 +0800

    refactor template and template_spec

--- refactor_example/template_spec.rb
+++ refactor_example/template_spec.rb
@@ -4,7 +4,8 @@ describe Template do
   include Template
 
   it "should substitute %CODE% and %ALTCODE% in the template" do
-    template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
+    # use updated expect syntax
+    expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')).to eq('Code is 5678901234; alt code is 56789-012')
   end
-  
+
 end
