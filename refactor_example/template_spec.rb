3ed7758c82bdea17acb03ece4e8e78ecda7cde8c
Nathan Matteson <nwmatteson@gmail.com>
2016-03-23 17:47:06 -0700

    initial commit

--- refactor_example/template_spec.rb
+++ refactor_example/template_spec.rb
@@ -4,7 +4,9 @@ describe Template do
   include Template
 
   it "should substitute %CODE% and %ALTCODE% in the template" do
-    template('Code is %CODE%; alt code is %ALTCODE%', '5678901234').should == 'Code is 5678901234; alt code is 56789-012'
+    # Refactored to meet newer rspec standard.
+    template_result = template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')
+    expect(template_result).to eq('Code is 5678901234; alt code is 56789-012')
   end
-  
+
 end
