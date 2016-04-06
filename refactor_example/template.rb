75e91ca9db28c772832208f65c79214910478135
Carson Swope <carsonmswope@gmail.com>
2016-04-05 18:04:32 -0700

    init rails project

--- refactor_example/template.rb
+++ refactor_example/template.rb
@@ -2,8 +2,11 @@ module Template
   def template(source_template, req_id)
 
     template_array = source_template.split('%')
-    template_array[1] = req_id
-    template_array[3] = req_id[0..4] + '-' + req_id[5..7]
+    # if the positions of %CODE% and %ALTCODE% never change, it should be
+    # template_array[1] = req_id
+    # template_array[3] = req_id[0..4] + '-' + req_id[5..7]
+    template_array[ template_array.index("CODE") ]    = req_id
+    template_array[ template_array.index("ALTCODE") ] = req_id[0..4] + '-' + req_id[5..7]
     template_array.join()
 
   end
