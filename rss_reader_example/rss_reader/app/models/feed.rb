7c8d1dabdbd4db1647f46d676059b86be052525b
holyoke <jasonebueng@gmail.com>
2017-02-07 16:56:17 -0800

    passes initial feed spec

--- rss_reader_example/rss_reader/app/models/feed.rb
+++ rss_reader_example/rss_reader/app/models/feed.rb
@@ -6,8 +6,18 @@
 #  title :text
 #  url   :string           not null
 #  note  :text
+require 'open-uri'
 
 class Feed < ActiveRecord::Base
   validates :url, presence: true, uniqueness: true
   validates :url, :url => true
+
+  after_create :set_title, on: [:create]
+
+  protected
+    def set_title
+      rss = SimpleRSS.parse(open(self.url, 'User-Agent' => 'ruby'))
+      #set title
+      self.update(title: rss.channel.title)
+    end
 end
--- rss_reader_example/rss_reader/spec/requests/feed_requests_spec.rb
+++ rss_reader_example/rss_reader/spec/requests/feed_requests_spec.rb
@@ -1,13 +1,13 @@
 require 'rails_helper'
+require 'byebug'
 
 RSpec.describe "Feed Requests", type: :request do
   let(:total) { 5 }
-  let!(:db_feeds) do
+  before do
     total.times do |x|
       url = "https://www.craigslist.org/about/best/all/index.rss"
       FactoryGirl.create(:feed, url: url + x.to_s)
     end
-    Feed.all
   end
 
 
@@ -15,20 +15,19 @@ RSpec.describe "Feed Requests", type: :request do
     get "/api/feeds"
 
     json = JSON.parse(response.body)
-    json_feeds = json["feeds"]
+    json_feeds = json["data"]
     expect(response).to be_success
     expect(json_feeds.length).to eq(total)
 
     # iterate and match each one
-    keys = FeedSerializer._attributes.compact
+    keys = FeedSerializer._attributes.compact - [:id]
 
     json_feeds.each_with_index do |feed, idx|
-      db_feed = db_feeds[idx]
-      keys.each do |key|
-        expect(feed[key.to_s]).to eq(db_feed[key])
-      end
+      db_feed = Feed.find feed["id"]
+        keys.each do |key|
+          expect(feed["attributes"][key.to_s]).to eq(db_feed[key])
+        end
     end
-
   end
 
   it "creates a feed" do
@@ -47,15 +46,7 @@ RSpec.describe "Feed Requests", type: :request do
       json = JSON.parse(response.body)
 
       expect(response).to be_success
-      expect(json["feed"]).to be_present
+      expect(json["data"]).to be_present
     end
-
-    it "has entries" do
-      get "/api/feeds/#{feed.id}"
-      json = JSON.parse(response.body)
-
-      expect(json["feed"]["entries"]).to be_present
-    end
-
   end
 end
