706523a22a7de72f806c526e4789845af0adc2c8
holyoke <jasonebueng@gmail.com>
2017-02-07 18:12:01 -0800

    sets up redux environment

---
+++ rss_reader_example/rss_reader/frontend/components/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const App = ({children}) => (
+  <div>
+    <h1>Rss Reader</h1>
+    {children}
+  </div>
+)
+
+export default App
---
+++ rss_reader_example/rss_reader/frontend/reducers/root_reducer.js
@@ -0,0 +1,7 @@
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  feed: []
+})
+
+export default rootReducer
