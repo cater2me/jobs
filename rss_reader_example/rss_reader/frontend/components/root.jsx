1ce2e7a4bd5be9ce198ee06ffd6f22e36af2090d
holyoke <jasonebueng@gmail.com>
2017-02-09 15:38:01 -0800

    adds onEnter api callback to route changes

--- rss_reader_example/rss_reader/frontend/components/root.jsx
+++ rss_reader_example/rss_reader/frontend/components/root.jsx
@@ -5,17 +5,31 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 import App from './App'
 import FeedsIndexContainer from './feeds/feeds_index_container'
 import FeedShowContainer from './feeds/feed_show_container'
+import * as feedActions from '../actions/feed_actions'
 
-const Root = ({store}) => (
-  <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/" component={App}>
-        <IndexRoute component={FeedsIndexContainer} />
-        <Route path="feeds/:id" component={FeedShowContainer} />
-        <Route path="feeds" component={FeedsIndexContainer} />
-      </Route>
-    </Router>
-  </Provider>
-)
+class Root extends React.Component {
+  constructor (props) {
+    super(props)
+    this.fetchFeed = this.fetchFeed.bind(this)
+  }
+  fetchFeed (nextState, replace) {
+    const { id } = nextState.params
+    store.dispatch(feedActions.fetchFeed(id))
+  }
+  render () {
+    const { store } = this.props
+    return (
+      <Provider store={store}>
+        <Router history={hashHistory}>
+          <Route path="/" component={App}>
+            <IndexRoute component={FeedsIndexContainer} />
+            <Route path="feeds/:id" component={FeedShowContainer} onEnter={this.fetchFeed} />
+            <Route path="feeds" component={FeedsIndexContainer} />
+          </Route>
+        </Router>
+      </Provider>
+    )
+  }
+}
 
 export default Root
