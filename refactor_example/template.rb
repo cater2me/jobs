90326d7abff457aa00076434fa22397aea24274b
holyoke <jasonebueng@gmail.com>
2017-02-16 16:56:59 -0800

    removes String.new declarations and structures variables
    adds #substitute_code specs
    
    refactors and extracts substitute code

--- refactor_example/template.rb
+++ refactor_example/template.rb
@@ -1,26 +1,24 @@
 module Template
   def template(source_template, req_id)
-    template = String.new(source_template)
+    template = source_template
+    code = req_id
+    altcode = code[0..4] + "-" + code[5..7]
 
     # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+    template = substitute_code(template, '%CODE%', code)
 
     # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+    template = substitute_code(template, '%ALTCODE%', altcode)
+
+    template
+  end
+
+  def substitute_code(template, code_str, code)
+    split_idx = template.index(code_str)
+    split_jdx = split_idx + code_str.size
+
+    template_part_one = template[0..(split_idx-1)]
+    template_part_two = template[split_jdx..template.length]
+    template_part_one + code + template_part_two
   end
 end
--- refactor_example/template_spec.rb
+++ refactor_example/template_spec.rb
@@ -7,4 +7,9 @@ describe Template do
   it "should substitute %CODE% and %ALTCODE% in the template" do
     expect(template('Code is %CODE%; alt code is %ALTCODE%', '5678901234')). to eq 'Code is 5678901234; alt code is 56789-012'
   end
+
+  it "should substitute a code into the string" do
+    expect(substitute_code('Code is %CODE%', '%CODE%', '5678901234')). to eq 'Code is 5678901234'
+    expect(substitute_code('; alt code is %ALTCODE%','%ALTCODE%','56789-012')). to eq '; alt code is 56789-012'
+  end
 end
